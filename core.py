2c6d66ac85afdfa5190af5bd20fdc7b18c78af58
Jordi-glitch <55662899+Jordi-glitch@users.noreply.github.com>
2021-04-25 10:29:24 +0800

    Add files via upload

---
+++ core.py
@@ -0,0 +1,34 @@
+import os, sys
+os.system("toilet -f pagga 'LOGIN' | lolcat")
+print
+username = 'Enigma'
+password = 'Gans'
+
+def restart():
+	ngulang =sys.executable
+	os.execl(ngulang, ngulang, *sys.argv)
+
+def main():
+	uname = raw_input("Username: ")
+	if uname == username :
+		pwd = raw_input("Password: ")
+
+		if pwd == password :
+			print "\033[1;33mLogin Berhasil"
+			sys.exit()
+
+		else:
+			print "\033[1;33mPassword Anda Salah"
+			print "\033[1;33mSilahkan Coba Lagi"
+			restart()
+
+	else:
+		print "\033[1;33mUsername & Password Anda Salah"
+		print "\033[1;33mSilahkan Coba Lagi"
+		restart()
+
+try:
+	main()
+except KeyboardInterrupt:
+	os.system('clear')
+	restart()
